63bd1b6cc72e294fb611512498db527e0fe253c0
Sam Tracey <g00398245@gmit.ie>
2021-02-01 14:42:19 +0000

    Weekly Tasks 02

---
+++ Week02/bmi.py
@@ -0,0 +1,14 @@
+# bmi.py
+# this code calculates a person's BMI or Body Mass Index
+# Author: Sam Tracey
+
+weight = int(input ("Please enter your weight in Kgs: "))
+height = int(input ("Please enter your height in Cm: "))
+
+# calculation used for BMI = weight divided by their height squared
+
+bmi = weight / ((height/100)**2)
+
+
+# rounding the output to the required number of decimal places and outputting the answer on screen
+print("BMI is " + str(round((bmi),2)))
