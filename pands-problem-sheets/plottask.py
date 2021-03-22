ae9435a1aadf933a2f5f14b2c68788e75ad08048
Sam Tracey <g00398245@gmit.ie>
2021-03-22 19:45:57 +0000

    Updated comments to include reference

--- pands-problem-sheets/plottask.py
+++ pands-problem-sheets/plottask.py
@@ -15,12 +15,14 @@ zpoints = xpoints **3
 
 # define characteristics of the graph (line styles, line colours, axis titles, legend etc.)
 
-plt.plot(xpoints, '*-r', label = 'f(x)=x')              # code for adding superscript labels found at https://stackoverflow.com/questions/21226868/superscript-in-python-plots
-plt.plot(ypoints, '+--b', label = 'g(x)=$x^2$' )
+plt.plot(xpoints, '*-r', label = 'f(x)=x') 
+# code for adding superscript labels found at https://stackoverflow.com/questions/21226868/superscript-in-python-plots            
+plt.plot(ypoints, '+--b', label = 'g(x)=$x^2$' )        
 plt.plot(zpoints, 'D-.g', label = 'h(x)=$x^3$')
 plt.title('Week 08 Weekly Task', fontname = 'Calibri', fontweight = 'bold', fontsize = '20')
 plt.ylabel("Transformed x value")
 plt.xlabel("x Value")
+# I decided to only use y axis grid as the graph was too noisy with both x & y axis grids added.
 plt.grid(axis = 'y')
 plt.legend()
 
